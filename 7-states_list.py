7c69d8b2fa7892ae38001de33449f39cedbcdbe5
Eugene Agesa <eugenethebandit@gmail.com>
2024-08-05 21:55:48 +0300

    creating a new view of cities to handle the RESTFUL api actions

--- 7-states_list.py
+++ 7-states_list.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-starts a Flask web application
+starting the flask web application
 """
 
 from flask import Flask, render_template
@@ -8,6 +8,18 @@ from models import *
 from models import storage
 app = Flask(__name__)
 
+
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    """displays a HTML page with the states listed in alphabetical order"""
+    states = sorted(list(storage.all("State").values()), key=lambda x: x.name)
+    return render_template('7-states_list.html', states=states)
+
+
+@app.teardown_appcontext
+def teardown_db(exception):
+    """closes the storage on teardown"""
+    storage.close()
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')
