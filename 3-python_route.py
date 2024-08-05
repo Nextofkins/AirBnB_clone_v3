a95bb9f1e037e48376634fb90c5f6a969fce6ce4
Eugene Agesa <eugenethebandit@gmail.com>
2024-08-05 20:56:08 +0300

    API status task

--- 3-python_route.py
+++ 3-python_route.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-starts a Flask web application
+starting the flask web application
 """
 
 from flask import Flask
@@ -9,26 +9,26 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def index():
-    """returns Hello HBNB!"""
+    """returns Hello HBNB! to the browser"""
     return 'Hello HBNB!'
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    """returns HBNB"""
+    """returns HBNB to the browser"""
     return 'HBNB'
 
 
 @app.route('/c/<text>', strict_slashes=False)
 def cisfun(text):
-    """display “C ” followed by the value of the text variable"""
+    """display s“C ” followed by the value of the text variable to the browser"""
     return 'C ' + text.replace('_', ' ')
 
 
 @app.route('/python', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def pythoniscool(text='is cool'):
-    """display “Python ”, followed by the value of the text variable"""
+    """displays “Python ”, followed value of the next variable to the browser"""
     return 'Python ' + text.replace('_', ' ')
 
 if __name__ == '__main__':
