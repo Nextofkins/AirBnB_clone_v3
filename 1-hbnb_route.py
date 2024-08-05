27f98f449fadb86aa0783b012205821fdec2f347
Eugene Agesa <eugenethebandit@gmail.com>
2024-08-05 20:36:57 +0300

    improving storage by updating the DBStorage and FileStorage

--- 1-hbnb_route.py
+++ 1-hbnb_route.py
@@ -1,9 +1,23 @@
 #!/usr/bin/python3
 """
-starts a Flask web application
+starting the flask web application
 """
 
+from flask import Flask
+app = Flask(__name__)
 
 
-if s__name__ == '__main__':
+@app.route('/', strict_slashes=False)
+def index():
+    """returns Hello HBNB to the browser!"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """returns HBNB to the browser"""
+    return 'HBNB'
+
+if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')
+
--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Fabric script that generates a tgz archive from the contents of the web_static
+Fabric script to generates a .tgz archive from the contents of the web_static
 folder of the AirBnB Clone repo
 """
 
@@ -9,4 +9,14 @@ from fabric.api import local
 from os.path import isdir
 
 
-
+def do_pack():
+    """generating a tgz archive file"""
+    try:
+        date = datetime.now().strftime("%Y%m%d%H%M%S")
+        if isdir("versions") is False:
+            local("mkdir versions")
+        file_name = "versions/web_static_{}.tgz".format(date)
+        local("tar -cvzf {} web_static".format(file_name))
+        return file_name
+    except:
+        return None
