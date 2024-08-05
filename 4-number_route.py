30ae3845997ff8e237385d17917f084e8d714d5c
Eugene Agesa <eugenethebandit@gmail.com>
2024-08-05 21:15:25 +0300

    task to create an endpoint to retreive number of each object type

--- 4-number_route.py
+++ 4-number_route.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-starts a Flask web application
+starting the flask web application
 """
 
 from flask import Flask
@@ -9,26 +9,27 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def index():
-    """returns Hello HBNB!"""
+    """returns Hello HBNB! to the browser"""
     return 'Hello HBNB!'
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    """returns HBNB"""
+    """returns HBNB to the browser"""
     return 'HBNB'
 
 
 @app.route('/c/<text>', strict_slashes=False)
 def cisfun(text):
-    """display “C ” followed by the value of the text variable"""
+    """display "C" and the text variable's value after that."""
     return 'C ' + text.replace('_', ' ')
+
 
 
 @app.route('/python', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def pythoniscool(text='is cool'):
-    """display “Python ”, followed by the value of the text variable"""
+    """show "Python" and the text variable's value after that."""
     return 'Python ' + text.replace('_', ' ')
 
 
@@ -39,3 +40,4 @@ def imanumber(n):
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')
+
